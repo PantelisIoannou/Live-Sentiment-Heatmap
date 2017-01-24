149d23cf66a4773f5ee49ca1a73bd30e421cf798
PantelisIoannou <panioan_13a@yahoo.gr>
2017-01-24 22:36:53 +0200

    new
    new

--- fronted/control.js
+++ fronted/control.js
@@ -1,26 +1,26 @@
 var module = angular.module('app', []);
 
 module.factory('socket', function($rootScope){
-    var socket = io.connect(window.location.href);
-  	return {
-        on: function (eventName, callback) {
-            socket.on(eventName, function () {
-            	var args = arguments;
-                $rootScope.$apply(function () {
-                    callback.apply(socket, args);
-                });
-            });
-        },
-        emit: function (eventName, data, callback) {
-	      	socket.emit(eventName, data, function () {
-	        	$rootScope.$apply(function () {
-	          		if (callback) {
-	            		callback.apply(socket, data);
-	          		}
-	        	});
-	      	})
-	    }
-    };
+var socket = io.connect(window.location.href);
+return {
+on: function (eventName, callback) {
+socket.on(eventName, function () {
+var args = arguments;
+$rootScope.$apply(function () {
+callback.apply(socket, args);
+});
+});
+},
+emit: function (eventName, data, callback) {
+socket.emit(eventName, data, function () {
+$rootScope.$apply(function () {
+if (callback) {
+	callback.apply(socket, data);
+}
+});
+})
+}
+};
 });
 
 module.factory('GoogleMap', function(){
@@ -156,78 +156,77 @@ module.factory('GoogleMap', function(){
 });
 
 module.controller('MapController', function($scope, GoogleMap, socket){
-	var searchkey = null;
-	var label = [];
-	$scope.showboundMessa = false;
-	$scope.searchkey = null;
+var searchkey = null;
+var label = [];
+$scope.showboundMessa = false;
+$scope.searchkey = null;
+$scope.buttonFinish = 'Stop';
+
+$scope.RestartBtnClick = function(){
+for (var i = 0; i < label.length; i++) {
+    label[i].setMap(null);
+}
+label = [];
+searchkey = $scope.searchkey.trim();
+}
+
+$scope.StopBtnClick = function(){
+if($scope.buttonFinish === 'Stop')
+	$scope.buttonFinish = 'Continue';
+else
 	$scope.buttonFinish = 'Stop';
+}
 
-	$scope.RestartBtnClick = function(){
-		for (var i = 0; i < label.length; i++) {
-            label[i].setMap(null);
-        }
-        label = [];
-		searchkey = $scope.searchkey.trim();
-	}
-
-	$scope.StopBtnClick = function(){
-		if($scope.buttonFinish === 'Stop')
-			$scope.buttonFinish = 'Continue';
-		else
-			$scope.buttonFinish = 'Stop';
-	}
-
-	socket.on('connected', function(){
-		socket.emit('start-streaming');
-	});
-	socket.on('new-tweet', function(tweet){
-		$scope.showboundMessa = false;
-		if($scope.buttonFinish === 'Stop' &&
-			(!searchkey || searchkey.length === 0 
-				|| tweet.text.toLowerCase().indexOf(searchkey.toLowerCase()) > -1))
-		{
-			var icon = null;
-			if(tweet.sentiment.score < 0)
-				icon = '/icon/negativemoticon.png';
-			if(tweet.sentiment.score == 0)
-				icon = '/icon/neutralemoticon.png';
-			if(tweet.sentiment.score > 0)
-				icon = '/icon/positivemoticon.png';
+socket.on('connected', function(){
+	socket.emit('start-streaming');
+});
+socket.on('new-tweet', function(tweet){
+$scope.showboundMessa = false;
+if($scope.buttonFinish === 'Stop' &&
+	(!searchkey || searchkey.length === 0 
+	|| tweet.text.toLowerCase().indexOf(searchkey.toLowerCase()) > -1))
+{
+var icon = null;
+if(tweet.sentiment.score < 0)
+	icon = '/icon/negativemoticon.png';
+if(tweet.sentiment.score == 0)
+	icon = '/icon/neutralemoticon.png';
+if(tweet.sentiment.score > 0)
+	icon = '/icon/positivemoticon.png';
 			
-			
-	      	var marker = new google.maps.Marker({
-	         	map: GoogleMap,
-	          	title: '@' + tweet.user.name,
-	         	position: new google.maps.LatLng(tweet.coordinates.coordinates[1], tweet.coordinates.coordinates[0]),
-	          	draggable: false,
-	         	animation: google.maps.Animation.DROP,
-				icon: icon
-	      	}); 
-	      	marker.setMap(GoogleMap);
-	      	label.push(marker);
+var marker = new google.maps.Marker({
+	map: GoogleMap,
+	title: '@' + tweet.user.name,
+	position: new google.maps.LatLng(tweet.coordinates.coordinates[1], tweet.coordinates.coordinates[0]),
+	draggable: false,
+	animation: google.maps.Animation.DROP,
+	icon: icon
+}); 
+marker.setMap(GoogleMap);
+label.push(marker);
 
-	      	var info = '<div class="row text-center tweet-info-window">'
-	      				+	'<div class="col-md-2 text-center">' 
-	      				+		'<img class="img-responsive" src=' + tweet.user.profile_image_url + '></img>'
-	      				+	'</div>'
-	      				+	'<div class="col-md-10 text-left">' 
-	      				+		'<p>' + '@' + tweet.user.name + '</p>'
-	      				+ 		'<p><strong>' + tweet.text + '</strong></p>'
-	      				+ 		'<p>' + tweet.created_at;
-	      	if(tweet.place){
-	      		info += '<br>' 
-	      				+ tweet.place.name + ', ' 
-	      				+ tweet.place.country;
-	      	}
-	      	info += '</p></div></div>';
+var info = '<div class="row text-center tweet-info-window">'
++	'<div class="col-md-2 text-center">' 
++		'<img class="img-responsive" src=' + tweet.user.profile_image_url + '></img>'
++	'</div>'
++	'<div class="col-md-10 text-left">' 
++		'<p>' + '@' + tweet.user.name + '</p>'
++ 		'<p><strong>' + tweet.text + '</strong></p>'
++ 		'<p>' + tweet.created_at;
+if(tweet.place){
+info += '<br>' 
++ tweet.place.name + ', ' 
++ tweet.place.country;
+}
+info += '</p></div></div>';
 
-	      	var infowindow = new google.maps.InfoWindow({
-	        	  content: info,
-	        	  maxWidth: 300
-	      	});
-	      	google.maps.event.addListener(marker, 'click', function(){
-	        	infowindow.open(GoogleMap, marker);
-	      	});
-      	}
-	});
+var infowindow = new google.maps.InfoWindow({
+	content: info,
+	maxWidth: 300
+});
+google.maps.event.addListener(marker, 'click', function(){
+infowindow.open(GoogleMap, marker);
+});
+}
+});
 });
